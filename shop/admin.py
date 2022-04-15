8396a3d4274ef948c966501957b74380a0b25e08
Kevin Kwak <kkwak@unomaha.edu>
2022-04-15 02:04:56 -0500

    admin setup for packages

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Package
+
+
 # Register your models here.
+class PackageList(admin.ModelAdmin):
+    list_display = ("pack_number", "location", "price", "start_date", "end_date", "rating",)
+    list_filter = ('pack_number', 'price', 'start_date', 'end_date',)
+    search_fields = ('pack_number', 'price', 'start_date', 'end_date',)
+    ordering = ['pack_number']
+
+
+admin.site.register(Package, PackageList)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from django.utils import timezone
+
+
 # Create your models here.
+class Package(models.Model):
+    location = models.CharField(max_length=50)
+    pack_number = models.IntegerField(blank=False, null=False)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(default=timezone.now, blank=True, null=True)
+    end_date = models.DateField(default=timezone.now, blank=True, null=True)
+    rating = models.IntegerField(blank=False, null=False)
+    weather = models.CharField(max_length=50)
+
+    def created(self):
+        self.recent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.pack_number)
