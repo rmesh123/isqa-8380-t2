7cdd8c9303595071b71398d912d2ecaf8f3ac28d
Kevin Kwak <kkwak@unomaha.edu>
2022-04-15 22:43:49 -0500

    homepage work

---
+++ shop/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Package
+
+
+class PackageForm(forms.ModelForm):
+    class Meta:
+        model = Package
+        fields = ('location', 'pack_number', 'description', 'price', 'start_date', 'end_date', 'rating', 'weather',)
--- shop/views.py
+++ shop/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import *
 from django.shortcuts import render, get_object_or_404
+from .forms import *
 from django.shortcuts import redirect
 
 now = timezone.now()
 
 
 def homepage(request):
+    package = Package.objects.filter(start_date__lte=timezone.now())
     return render(request, 'homepage.html',
-                  {'shop': homepage})
+                  {'packages': package})
 
 
 def product(request):
