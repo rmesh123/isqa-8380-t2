a518f926b1f1966a5168b50b0bf94eb8322e891f
rmesh123 <rdanuwar1994@gmail.com>
2022-04-17 22:06:48 -0500

    forgot/ Reset/change password done

--- shop/urls.py
+++ shop/urls.py
@@ -2,10 +2,30 @@ from django.urls import re_path as url
 from . import views
 from django.urls import path
 from .views import *
+from django.conf.urls import url
+from . import views
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.contrib.auth import views as auth_views
 
 app_name = 'shop'
 urlpatterns = [
     path('<int:pk>', PackageDetailView.as_view(), name='product'),
     path('', PackageListView.as_view(), name='homepage'),
     path('signup', signup, name='signup'),
+
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/forgot_password.html"), name='forgot_password'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name='password_reset_complete'),
+
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name = "registration/changedpassword.html"), name='changedpassword'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name = "registration/changePassword.html"), name='changePassword'),
+
+
+
+
+
+
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -9,6 +9,23 @@ from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import get_template
+from .models import *
+from .forms import *
+from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.generic import CreateView, ListView
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+
+
+
 now = timezone.now()
 
 
@@ -35,6 +52,14 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def forgot_password(request):
+    if request.method == 'POST':
+        return render(request, 'shop/templates/registration/password_reset_complete.html')
+    else:
+        return render(request, 'shop/templates/registration/password_reset_complete.html')
+
+
 
 
 class PackageDetailView(DetailView):
